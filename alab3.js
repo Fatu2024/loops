37b622caa6eb9f35c23d9cc5fbbe84cba660b454
Fatu2024 <fatumab3@gmail.com>
2024-02-26 20:14:50 -0500

    part 1 redone

--- alab3.js
+++ alab3.js
@@ -7,19 +7,23 @@
 // If a number is divisible by 5, log “Buzz.”
 // If a number is divisible by both 3 and 5, log “Fizz Buzz.”
 // If a number is not divisible by either 3 or 5, log the number.
-for (let i = 0; i <= 100; i++){
-    console.log(i);
-    if (i %3 === 0) {
-        console.log("fizz");
-    if (i %5 === 0) {
-        console.log("buzz");
-    if (i %3 === 0 && i %5 === 0){
-        console.log("fizz buzz")
-    if (i %3 !== 0 && i %5 !== 0) {
+for (let i = 1; i <= 100; i++) {
+    if (i %3 === 0 && i %5 === 0) {
+        console.log('fizz buzz');
+    } else if (i %3 === 0) {
+        console.log('fizz');
+    }else if (i %5 === 0) {
+        console.log('buzz');
+    } else {
         console.log(i)
     }
-    }
-
-    }
-    }
 }
+
+
+// Write a script that accomplishes the following:
+// Declare an arbitrary number, n.
+// Create a loop that searches for the next prime number, starting at n and incrementing from there.
+// As soon as you find the prime number, log that number and exit the loop.
+// for (let i =1; i <= 10; i++){
+//     if (isPrime)
+// }
